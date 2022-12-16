7d8f5e79b7b815d6ac22d07ef2cd43568793e683
OthmenManeri <91779774+OthmenManeri@users.noreply.github.com>
2022-12-16 13:10:19 +0100

    Add files via upload

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:634480644405:web:5549258ea987afaefb7c7c";
+  static String apiKey = "AIzaSyCh4tOnYtmibHvDhOgGS5a2B_5NAcdSMX8";
+  static String messagingSenderId = "621009245562";
+  static String projectId = "chatappflutter-47845";
+  final primaryColor = const Color(0xFFee7b64);
+}
